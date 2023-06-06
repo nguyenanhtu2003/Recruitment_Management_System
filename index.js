fae8aca4135465a7d027d1b77d6cca47e715b6d4
Thinhnt19 <thinhntpd05744@fpt.edu.vn>
2023-06-06 20:08:36 +0700

    Revert "6/6/2023-7h03"
    This reverts commit facab8321ebc6a6499ca81ccdb36cd82e26e6aad.

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const web = require("./routes/web.router");
+
 
 const port = process.env.PORT || 3000;
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-app.use("/", web);
 
 app.listen(port, () => {
-  console.log(` Connect Success Port http://localhost:${port}`);
-});
+    console.log(` Connect Success Port http://localhost:${port}`);
+  });
