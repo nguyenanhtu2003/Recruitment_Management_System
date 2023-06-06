cdfdaf6242529342bd817c4a94cbde048c809207
quốc huy lê <huylquoc19102003@gmail.com>
2023-06-06 18:03:05 +0700

    Update code

---
+++ config/connect.config.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2/promise");
+
+const db = mysql.createPool({
+  host: "sql12.freesqldatabase.com",
+  user: "sql12621730",
+  password: "1rnvkBZ1Zu",
+  port: 3306,
+  database: "sql12621730",
+});
+
+if (!db) {
+  console.log("Lỗi Rồi");
+}
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+    console.log(` Connect Success Port http://localhost:${port}`);
+  });
