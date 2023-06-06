deac302c4155ee6e1c9f11bdad1fedf69c6cc578
quốc huy lê <huylquoc19102003@gmail.com>
2023-06-06 21:23:16 +0700

    6/6/2023-9h23

--- routes/web.router.js
+++ routes/web.router.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const indexController = require("../controllers/index.controller");
+
+router.get("/", indexController.getIndex);
 
 module.exports = router;
