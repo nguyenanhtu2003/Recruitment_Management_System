5550612f9aeeb2977314ae5b5f2b8efeb6d0fb7b
huy <huy@gmail.com>
2023-06-07 10:16:48 +0700

    7/6/2023-10h16

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-
+const web = require("./routes/web.router");
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("public"));
-
+app.use("/", web);
 
 
 app.listen(port, () => {
